d980831ad2bcf2510871ca0d4d90b609c57ac2f0
Danilo Ramirez <danilo0613@hotmail.com>
2024-09-12 23:48:41 -0500

    Se crean controllers de obtención de post y se corrige el create

---
+++ Blog/Models/InnerModels/PostWithAuthor.cs
@@ -0,0 +1,10 @@
+﻿using Blog.Models.DataSet;
+
+namespace Blog.Models.InnerModels
+{
+    public class PostWithAuthor
+    {
+        public Post Post { get; set; }
+        public string AuthorName { get; set; }
+    }
+}
