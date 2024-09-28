66a71e4292656a73bd6dc90cbca2f9a7580927cc
Danilo Ramirez <danilo0613@hotmail.com>
2024-09-27 19:10:49 -0500

    Create like - unlike - get liked post by users - endpoints

---
+++ Blog/Profiles/LikeProfile.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using Blog.Models.DataSet;
+using Blog.Models.Dtos.Request;
+
+namespace Blog.Profiles
+{
+    public class LikeProfile : Profile
+    {
+        public LikeProfile()
+        {
+            // Mapeo de LikeRequestDto a Like
+            CreateMap<LikeRequestDto, Like>()
+                .ForMember(dest => dest.PostId, opt => opt.MapFrom(src => src.PostId))
+                .ForMember(dest => dest.UserId, opt => opt.MapFrom(src => src.UserId));
+        }
+    }
+
+}
